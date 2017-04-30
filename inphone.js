1e0e3418c4b9c7c295b9bd8fa12599b5c759b04d
velosepappe <sam.vanloocke@gmail.com>
2017-04-30 07:56:31 +0200

    eerste test
    begin van een demo endpoint lijst in inphone.js

--- inphone.js
+++ inphone.js
@@ -1,6 +1,56 @@
 function someCallback (data) {
-			console.log(data);
-	};
+	console.log(data);
+};
+
+var endpoints;
+function getEndpointStatus(){
+	var demoEndpoints = [
+	  {
+		"technology": "SIP",
+		"resource": "sam",
+		"state": "online",
+		"channel_ids": []
+	  },
+	  {
+		"technology": "SIP",
+		"resource": "webrtc",
+		"state": "unknown",
+		"channel_ids": []
+	  },
+	  {
+		"technology": "SIP",
+		"resource": "tlspc",
+		"state": "unknown",
+		"channel_ids": []
+	  },
+	  {
+		"technology": "IAX2",
+		"resource": "demo",
+		"state": "unknown",
+		"channel_ids": []
+	  },
+	  {
+		"technology": "SIP",
+		"resource": "tlsandroid",
+		"state": "online",
+		"channel_ids": []
+	  }
+	];
+	return demoEndpoints;
+}
+
+function updateStatus(){
+	endpoints = getEndpointStatus();
+	  var items = [];
+	$.each( endpoints, function( key, val ) {
+		items.push( "<div id='" + val.resource + "' class='endpoint endpoint_state_" + val.state + "'>" + val.resource + " : " + val.state + "</div>" );
+	});
+ 
+  $( "<div/>", {
+    "id": "endpointList",
+    html: items.join( "" )
+  }).appendTo( "div#main" );
+}
 
 $(document).ready(function(){
 	console.log("Work allready!");
@@ -11,10 +61,11 @@ $(document).ready(function(){
 			'api_key': 'samme:demo'
 		},
 	  dataType: 'json',
-	  Origin:'ari.asterisk.org',
 	  result:someCallback,
 		error: function(xhr, status, error) {
 			console.log(status + '; ' + error);
 		}
 	});
+	
+	updateStatus();
 });
